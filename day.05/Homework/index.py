c6d51fa27ab4f6b10288541ae0d2b02fa8bc3e37
nukigagnidze <marisefashvili32@gmail.com>
2024-10-14 07:46:38 -0700

    first commit

---
+++ day.05/Homework/index.py
@@ -0,0 +1,12 @@
+name = input("Please enter your name : ")
+print(name)
+
+surname = input("Please enter your surname: ")
+print(surname)
+
+conchre = input("Please enter your conchre")
+print(conchre)
+
+email = input("Please enter your email")
+print(email)
+
