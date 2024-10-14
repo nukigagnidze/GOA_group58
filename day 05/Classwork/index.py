6d8d36ffe5b161ee22f1430420f76df8c5151c0e
nukigagnidze <marisefashvili32@gmail.com>
2024-10-14 07:05:41 -0700

    first commit

---
+++ day 05/Classwork/index.py
@@ -0,0 +1,60 @@
+# 1
+# name = "Alice"
+# print(name)
+
+# # 2
+# salary = 75000
+# age = 30
+# role = "Software Engineer"
+# print(salary)
+
+# # 3
+# balance = 750
+# cost = 400
+# print(balance - cost)
+
+# coments
+# 1
+# num1 = 10
+# num2 = 5
+# print(num1 + num2)
+
+# აქ გამოყენებულია num1, num2 და print-ს და num1-ს და num2-ს მიუმატებს
+
+# 2
+# first_name = "John"
+# last_name = "Doe"
+# full_name = first_name + " " + last_name
+# print(full_name)
+
+# აქ გამოვიყენე first_name, last_name, full_name და print ამ კოდში გამოიტანს John Doe-ის
+
+# 3
+# num1 = 10
+# num2 = 5
+# print(num1 + num2)
+
+# აქ გამოყენებულია num1, num2 და print-ს და num1-ს და num2-ს მიუმატებს
+
+# 4
+# temperature = 30
+# print("temperature")
+
+# აქ გამოყენებულია temperature და print-ი და აქ გამოიტანს 30-ს
+
+
+# 4
+age = input("Please enter your age: ")
+print("your age is " + str(age))
+
+# 5
+favoret_color = input("Please enter your favoret_color: ")
+print("your favoret_color is " + str(favoret_color))
+
+# 6
+fevorite_car = input("Please enter your favorete_car: ")
+print("your favoret_car is " + str(fevorite_car))
+
+# 7
+age = input("Please enter your age: ")
+print("your age is " + str(age))
