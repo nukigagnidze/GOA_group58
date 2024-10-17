acbc779e046c5ce01fb2051271057e7b3a049bf3
nukigagnidze <marisefashvili32@gmail.com>
2024-10-17 11:00:25 -0700

    first commit

---
+++ day.07/Homework/index.py
@@ -0,0 +1,86 @@
+# 1
+# sololerni gvqonda
+
+# 2
+# name = "nuki"
+# print(name)
+
+# surname = "gagnidze"
+# print(surname)
+
+# age = 10
+# print(age)
+
+# 3
+# number = input("Please enter your number: ")
+# print("your age is " + int(number))
+
+# 4
+# result = 5 < 2
+# print(result)
+
+# result1 = 5 < 27
+# print(result1)
+
+# result2 = 12345 < 34567
+# print(result2)
+
+# result = 2346 < 1234
+# print(result)
+
+# result = 1234 > 2234
+# print(result)
+
+# result = 23456789 < 2345678
+# print(result)
+
+# result = 5 > 2
+# print(result)
+
+# result = 5345678 < 2457
+# print(result)
+
+# result = 5 > 2234
+# print(result)
+
+# 5
+# result = 534567890 < 2345678 or 534567 > 6543
+
+# print(result)
+
+# result = 765 < 3456 and 46 > 456
+
+# print(result)
+
+# 6
+# age = input("Please enter your age: ")
+# print("your age is " + int(age))
+
+# age = input("Please enter your age: ")
+# print("your age is " + float(age))
+
+# 7
+# number = int(input("Please enter your number: "))
+
+# if number >=37:
+#     print("its even")
+# else:
+#     print("its odd")
+
+# 8
+# (1)
+# age = int(input("Please enter your age: "))
+
+# if age >=18:
+#     print("you are adult")
+# else:
+#     print("you are kid")
+
+# (2)
+# name = int(input("Please enter your name: "))
+
+# if name >=4:
+#     print("you are a mach")
+# else:
+#     print("you are not a mach")
+
