49f09035fcc750451713b7cdd59703814e196ee1
nukigagnidze <mrisefashvili32@gmail.com>
2024-10-28 11:42:18 -0700

    first commit

--- day.12/Homework/index.py
+++ day.12/Homework/index.py
@@ -1,7 +1,5 @@
 # 1
-# counter = 0
-# while counter < 51:
-#       print(counter)
+# i = 0
 
 #       counter += 1
 
@@ -23,6 +21,20 @@
 #     print("Result is Odd")
 
 # 3
+even_sum = 0
+odd_sum = 0
+
+for i in range(1, 21):
+    if i % 2 == 0:
+        even_sum = even_sum + i
+    else:
+        odd_sum = odd_sum + i
+
+
+if even_sum ** 5 > odd_sum ** 5:
+    print("Even sum is greater than odd sum", even_sum ** 5)
+else:
+    print("Odd sum is greater than even sum", odd_sum ** 5)
 
 # 4
 # am kodshi gamoitans True-s
---
+++ day.13/Homework/index.py
@@ -0,0 +1,43 @@
+# 1
+# sololerni iyo
+
+# 2
+# numbers = []
+
+# for i in range(5):
+#     number = int(input("Enter a number: "))
+#     numbers.append(number)
+
+# print(numbers)
+
+# 3
+# fruits = ["apple", "banana", "cherry", "orange"]
+
+# last_index = len(fruits) - 1
+
+# user_fav = input("Enter your favorite fruit: ")
+
+# if last_index % 2 == 0:
+#     fruits.append(user_fav)
+
+# print(fruits)
+
+# 4
+# names = []
+
+# for i in range(5):
+#     name = input("Enter your name: ")
+    
+#     if len(name) >= 5:
+#         names.append(name)
+#     else:
+#         print("Name is too short")
+
+
+# 5
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# print(numbers)
+
+# index = int(input("Please enter a number: "))
+# numbers.pop(index - 1)
+# print(numbers)
