dc12fd3a7a1e8a2ac019aa1c6379665d035bec29
nukigagnidze <marisefashvili32@gmail.com>
2024-11-04 03:04:38 -0800

    first commit

---
+++ day.16/Homework/index.py
@@ -0,0 +1,30 @@
+# 1
+# counter = 0
+# while counter < 21:
+#       print(counter)
+
+#       counter += 1
+
+# 2
+# counter = 0
+# while counter < 101:
+#       print(counter)
+
+#       counter += 1
+
+# 3
+# counter = 0
+# while counter < 501:
+#       print(counter)
+
+#       counter += 1
+
+# 4
+# list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# counter = 0
+# while counter < 10:
+#       print(counter)
+
+#       counter += 1
+
+# 5
