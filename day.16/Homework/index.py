9b854dc853f31fd7107055e013128c88178f472a
nukigagnidze <marisefashbili32@gmail.com>
2024-11-05 11:05:35 -0800

    goa best

--- day.16/Homework/index.py
+++ day.16/Homework/index.py
@@ -13,11 +13,12 @@
 #       counter += 1
 
 # 3
-# counter = 0
-# while counter < 501:
-#       print(counter)
+# numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# factorial=1
+# for number in numbers:
+#     factorial *= number
 
-#       counter += 1
+#     print(factorial)
 
 # 4
 # list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -28,3 +29,56 @@
 #       counter += 1
 
 # 5
+# numbers = [5, -7, 3, 2, -11, 6, 8, -10, 12, 15, -17, 19, -21]
+
+# negative_numbers = []
+
+# index = 0
+
+# while index < len(numbers):
+#     if numbers[index] < 0:
+#         negative_numbers.append(numbers[index])
+#     index += 1
+
+# print(negative_numbers)
+
+# 7
+# chars = ["a", "a", "a", "b", "c", "b", "c"]
+
+# no_duplicates = []
+
+# for char in chars:
+#     if no_duplicates.count(char) == 0:
+#         no_duplicates.append(char)
+
+# chars = ["a", "a", "a", "b", "c", "b", "c"]
+
+# no_duplicates = []
+
+# for char in chars:
+#     if char not in no_duplicates:
+#         no_duplicates.append(char)
+
+# print(no_duplicates)
+
+# 9
+number = int(input("Enter a number: "))
+
+divisors = []
+
+# for i in range(1, number + 1):
+# if number % i == 0:
+# divisors.append(i)
+
+# print(divisors)
+
+# 10
+years = int(input("Enter the number of years: "))
+
+century = years // 100
+remaining_years = years % 100
+
+if remaining_years == 0:
+    print(century)
+else:
+    print(century + 1)
---
+++ day.18/Homework/index.py
@@ -0,0 +1,48 @@
+# 1 
+# sololerin iyo
+
+# 2
+
+# 3
+# counter = 0
+# while counter < 21:
+#       print(counter)
+
+#       counter += 1
+
+# 4
+# names = ["ani", "anamaria", "anuki", "akako"]
+
+# print(names[1:3])
+
+# 5
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(numbers[1:5])
+
+# 6
+# numbers = int(input("Please enter your number: "))
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(numbers[1;8])
+
+# 7
+# num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(num1[2:5])
+
+# 8
+# num2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(num2[2:5])
+
+# 9
+# num3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(num3[2:5])
+
+# 10
+# num3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(num3[0:0])
