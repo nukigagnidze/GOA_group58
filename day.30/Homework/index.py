f2eaba3c135ee6cb09e26797c3b469e4e3a255c7
nukigagnidze <marisefashvili>
2024-11-28 07:55:54 -0800

    first commit

---
+++ day.30/Homework/index.py
@@ -0,0 +1,9 @@
+def manual_count(nums_list, number):
+    count = 0
+
+    for i in nums_list:
+        if i == number:
+            count += 1
+    
+    return count
+print(manual_count([1, 1, 1, 2, 3, 4, 5], 3))
