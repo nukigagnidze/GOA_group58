6bfc33a875f9ed52b443f74201d9e28784147490
nukigagnidze <marisefashvili>
2024-12-11 04:37:49 -0800

    GOA BEST

--- day.36/Homework/index.py
+++ day.36/Homework/index.py
@@ -1,1 +1,29 @@
 # aq codewarsi gvqonda
+# textarea:focus, input:focus{
+
+# outline: 0;
+
+# }
+
+# .form-line-error input:not(#coupon-input), .form-line-error textarea {
+
+# border: 1px solid #FF3030;
+
+# -moz-box-shadow: 0 0 0px #FF3030;
+
+# -webkit-box-shadow: 0 0 0px #FF3030;
+
+# box-shadow: 0 0 0px #FF3030;
+
+# background-color: #FFFFFF;
+
+# }
+
+registered_pass = "1234567891011121314151617181920"
+user_input = ""
+
+while user_input != registered_pass:
+    user_input = input("Enter password: ")
+
+def revers_words(string1):
+    splited_str = string1.split()
---
+++ day.37/Homework/index.py
@@ -0,0 +1,1 @@
+# syyle - atributi cvlis color-s, bacraund-colors da bevr uamravs
