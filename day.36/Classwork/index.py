c74fe36df657c964a72a1b2e7c1df49f40146300
nukigagnidze <marisefashvili>
2024-12-23 07:06:32 -0800

    GOA BEST!

--- day.36/Classwork/index.py
+++ day.36/Classwork/index.py
@@ -1,8 +1,8 @@
 # 1) კარგად დაუკვირდით შემდეგ კოდს და ახსენით თითოეული მოქმედება კომენტარებით:
 
-# def manual_range(end, start = 0, step = 1):
-#     while start <= end:
-#         print(start)
-#         start += step
+def manual_range(end, start = 0, step = 1):
+    while start <= end:
+        print(start)
+        start += step
     
-# manual_range(10, 1, 2)
+manual_range(10, 1, 2)
